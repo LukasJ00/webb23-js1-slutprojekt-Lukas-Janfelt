f4870c823913a03081f53512d971019c5a1ed635
LukasJ00 <lukas@janfelt.se>
2023-05-26 12:35:25 +0200

    new

--- JS/script.js
+++ JS/script.js
@@ -1,96 +1,132 @@
-const form = document.querySelector('form');
-const searchResultDiv = document.querySelector('#container')
+const form = document.querySelector('#weatherForm');
+const searchResultDiv = document.querySelector('#result-container');
 const errorH3 = document.querySelector('#error');
 
+form.addEventListener("submit", fetchWeather);
 
+function showWeather(weather) {
+    console.log(weather);
+    const city = document.createElement('h2');
+    city.textContent = weather.name; 
+    searchResultDiv.append(city);
 
-form.addEventListener("submit", searchCityWeather);
+    const temp = document.createElement('h3');
+    temp.textContent = "Temp: " + weather.main.temp + " °C";
+    searchResultDiv.append(temp);
 
-function searchCityWeather(event){
-    event.preventDefault();
-    const apiKey = "e26f81e1cb4b35fdc36b4b6cd3ea2ca5";
-    const cityInput = document.querySelector('input[type="text"]');
-    const city = cityInput.value;
-    const wheatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-    
+    const humidity = document.createElement('h3');
+    humidity.textContent = "Humidity: " + weather.main.humidity + " %";
+    searchResultDiv.append(humidity);
 
-    fetch(wheatherUrl)
-    .then((response)=> response.json())
-    .then((data) => console.log(data));
-  
+    const wind = document.createElement('h3');
+    wind.textContent = "Wind: " + weather.wind.speed + " mph, " + weather.wind.deg + "°";
+    searchResultDiv.append(wind);
 }
 
-searchCityWeather();
+    function showErrorMessage(message) {
+    errorH3.textContent = message;
+}
+
+function fetchWeather(event) {
+    event.preventDefault();
+    searchResultDiv.innerHTML = ''; 
+    errorH3.textContent = '';
+    const city = event.target.elements[0].value;
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=e26f81e1cb4b35fdc36b4b6cd3ea2ca5`;
+   
+    fetch(weatherUrl)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('City not found');
+            }
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            showWeather(data);
+        })
+        .catch((error) => {
+            console.log(error);
+            showErrorMessage('City not found');
+        });
+}
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const form = document.querySelector('form');
-// const searchResultDiv = document.querySelector('#container');
-// const errorH3 = document.querySelector('#error');
-
-// form.addEventListener('submit', function(event){
-//     event.preventDefault();
-//     searchResultDiv.innerHTML = '';
-//     errorH3.innerText = '';
-
-//     const input = document.querySelector('input');
-//     const language = input.value;
-//     form.reset();
 
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //     const form = document.querySelector('#weatherForm');
+    //     const searchResultDiv = document.querySelector('#result-container');
+    //     const errorH3 = document.querySelector('#error');
 
-//     const countryUrl = 'https://restcountries.com/v3.1/lang/' + language;
-//     // console.log(countryUrl);
 
+    //     function showWheather(weather){
+    //         console.log(weather);
+    //         const city = document.createElement('h2');
+    //         city.textContent = weather.name; 
+    //         resultContainer.append(city);
 
-//     fetch(countryUrl)
-//         .then( response=>{
-//             // console.log(response);
-//             if(response.ok){
-//                 return response.json();
-//             }
-//             else{
-//                 throw 'Something went wrong';
-//             }
-//         })
-//         .then( displayCountries )
-//         .catch( handleError );
-// })
+    //         const temp = document.createElement('h3');
+    //         temp.textContent = ("Temp:" + weather.main.temp + " °C");
+    //         resultContainer.append(temp);
 
-// function displayCountries(countries){
-//     // console.log(countries[0].name.common);
+    //         const humidity = document.createElement('h3');
+    //         humidity.textContent = ("Humidity:" + weather.main.humidity + " %");
+    //         resultContainer.append(humidity);
 
-//     countries.forEach(country => {
-//         console.log(country.name.common);
-//         const p = document.createElement('p');
-//         p.innerText = country.name.common;
-//         searchResultDiv.append(p);
-//     });
-// }
+    //         const wind = document.createElement('h3');
+    //         wind.textContent = ("Wind:" + weather.wind.speed + " mph" + weather.wind.deg + "°" );
+    //         resultContainer.append(wind);
+    //     }
+    
+       
+     
 
-// function handleError(error){
-//     // console.log(error);
-//     errorH3.innerText = error;
-// }
+    
+    //       function fetchWeather(event){
+    //         const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${event}&units=matric&appid=e26f81e1cb4b35fdc36b4b6cd3ea2ca5`
+           
+    //         fetch(weatherUrl)
+    //                 .then((response) => response.json())
+    //                 .then((data) => {
+    //                     console.log(data);
+    //                    })
+    //                 .catch((error) => {
+    //                     console.log(error);
+    //                 });
+    //         }
+        
+    // fetchWeather('Malmö');
+     
+     
+     
+     
+     
+     
+     
