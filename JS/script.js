6143316f5ad32263e230bc4602aada84ba2e32b8
LukasJ00 <lukas@janfelt.se>
2023-05-30 21:41:37 +0200

    new

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,4 @@
+// Hämta referenser till HTML-element
 const form = document.querySelector('#weatherForm');
 const searchResultDiv = document.querySelector('#result-container');
 const errorH3 = document.querySelector('#error');
@@ -7,31 +8,36 @@ const intervalInput = document.querySelector('#interval-input');
 intervalInput.setAttribute('max', '12');
 const cityNameElement = document.getElementById('city-name');
 
+// Lägg till händelselyssnare för formuläret och knapparna
 form.addEventListener('submit', fetchWeather);
 increaseButton.addEventListener('click', increaseInterval);
 decreaseButton.addEventListener('click', decreaseInterval);
 intervalInput.addEventListener('change', updateInterval);
 
+// Variabler för väderintervallet och stad
 let forecastInterval = 12; // Standardintervall på 12 timmar
 const minForecastInterval = 3; // Minsta intervall på 3 timmar
 let city = '';
 
+// Funktion för att visa väderinformation
 function showWeather(weather) {
-  console.log(weather);
-
+  // Rensa tidigare resultat och felmeddelanden
   searchResultDiv.innerHTML = '';
   errorH3.textContent = '';
 
+  // Visa stadens namn
   cityNameElement.textContent = weather.city.name;
 
   // Skapa element för nuvarande väder
   const currentWeatherContainer = document.createElement('div');
   currentWeatherContainer.classList.add('forecast-item');
 
+  // Skapa element för nuvarande väderbeskrivning
   const currentWeatherTitle = document.createElement('h3');
   currentWeatherTitle.textContent = 'Current Weather';
   currentWeatherContainer.append(currentWeatherTitle);
 
+  // Skapa element för temperatur, luftfuktighet och vindhastighet
   const currentWeatherTemperature = document.createElement('p');
   currentWeatherTemperature.textContent = `Temp: ${weather.list[0].main.temp} °C`;
   currentWeatherContainer.append(currentWeatherTemperature);
@@ -44,6 +50,7 @@ function showWeather(weather) {
   currentWeatherWindSpeed.textContent = `Wind Speed: ${weather.list[0].wind.speed} m/s`;
   currentWeatherContainer.append(currentWeatherWindSpeed);
 
+  // Visa ikon och beskrivning för nuvarande väder
   const currentWeatherInfo = weather.list[0].weather[0];
   if (currentWeatherInfo && currentWeatherInfo.icon) {
     const iconUrl = `https://openweathermap.org/img/wn/${currentWeatherInfo.icon}.png`;
@@ -55,9 +62,10 @@ function showWeather(weather) {
     currentWeatherContainer.append(iconInfo);
   }
 
+  // Lägg till nuvarande väder i resultatet
   searchResultDiv.append(currentWeatherContainer);
 
-  
+  // Skapa väderprognos för varje intervall
   for (let i = 3; i <= forecastInterval; i += 3) {
     const forecastItem = weather.list[(i / 3) - 1];
     const forecastItemTime = new Date(forecastItem.dt * 1000);
@@ -96,18 +104,23 @@ function showWeather(weather) {
       forecastItemContainer.append(iconInfo);
     }
 
+    // Lägg till väderprognos i resultatet
     searchResultDiv.append(forecastItemContainer);
   }
 
+  // Visa resultatet
   searchResultDiv.style.display = 'flex';
 }
 
+// Funktion för att visa felmeddelande
 function showErrorMessage(message) {
+  // Rensa tidigare resultat och felmeddelanden
   searchResultDiv.innerHTML = '';
   errorH3.textContent = message;
   searchResultDiv.style.display = 'none';
 }
 
+// Funktion för att hämta väderdata
 function fetchWeather(event) {
   event.preventDefault();
   searchResultDiv.innerHTML = '';
@@ -129,17 +142,19 @@ function fetchWeather(event) {
     .catch((error) => {
       console.log(error);
       showErrorMessage('City not found');
-      searchResultDiv.innerHTML = ''; 
-      cityNameElement.textContent = ''; 
+      searchResultDiv.innerHTML = '';
+      cityNameElement.textContent = '';
     });
 }
 
+// Funktion för att öka väderintervallet
 function increaseInterval() {
   forecastInterval += 3;
   updateIntervalInput();
   fetchWeatherByCity(city);
 }
 
+// Funktion för att minska väderintervallet
 function decreaseInterval() {
   if (forecastInterval - 3 >= minForecastInterval) {
     forecastInterval -= 3;
@@ -148,6 +163,7 @@ function decreaseInterval() {
   }
 }
 
+// Funktion för att uppdatera väderintervallet
 function updateInterval() {
   const newForecastInterval = parseInt(intervalInput.value);
   if (newForecastInterval >= minForecastInterval) {
@@ -156,10 +172,12 @@ function updateInterval() {
   }
 }
 
+// Funktion för att uppdatera väderintervallets inputfält
 function updateIntervalInput() {
   intervalInput.value = forecastInterval.toString();
 }
 
+// Funktion för att hämta väderdata med en specifik stad
 function fetchWeatherByCity(city) {
   const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=e26f81e1cb4b35fdc36b4b6cd3ea2ca5`;
 
@@ -177,8 +195,8 @@ function fetchWeatherByCity(city) {
     .catch((error) => {
       console.log(error);
       showErrorMessage('City not found');
-      searchResultDiv.innerHTML = ''; 
-      cityNameElement.textContent = ''; 
+      searchResultDiv.innerHTML = '';
+      cityNameElement.textContent = '';
     });
 }
 
@@ -186,12 +204,20 @@ function fetchWeatherByCity(city) {
 
 
 
-
-
 // ändra så att forecasten först visar hur vädret ser ut just nu, sedan utgår ifrån intervallerna 3 timmar framåt beronde på vald intervall. upp til 12 timmar. eventuellt ha vädret just nu under search weather och sen se timmar framåt.  
+// mall 
+// function fetchWeather(query){
+//   const url = `https://api.openweathermap.org/data/2.5/weather?q=London&units=matric&appid=e26f81e1cb4b35fdc36b4b6cd3ea2ca5`
+// fetch(url)
+// .then((response) => response.json())
+// .then((data) => console.log(data));
+// }
+
+// fetchWeather();
 
 
 
+// // Hämta referenser till HTML-element
 // const form = document.querySelector('#weatherForm');
 // const searchResultDiv = document.querySelector('#result-container');
 // const errorH3 = document.querySelector('#error');
@@ -201,31 +227,36 @@ function fetchWeatherByCity(city) {
 // intervalInput.setAttribute('max', '12');
 // const cityNameElement = document.getElementById('city-name');
 
+// // Lägg till händelselyssnare för formuläret och knapparna
 // form.addEventListener('submit', fetchWeather);
 // increaseButton.addEventListener('click', increaseInterval);
 // decreaseButton.addEventListener('click', decreaseInterval);
 // intervalInput.addEventListener('change', updateInterval);
 
+// // Variabler för väderintervallet och stad
 // let forecastInterval = 12; // Standardintervall på 12 timmar
 // const minForecastInterval = 3; // Minsta intervall på 3 timmar
 // let city = '';
 
+// // Funktion för att visa väderinformation
 // function showWeather(weather) {
-//   console.log(weather);
-
+//   // Rensa tidigare resultat och felmeddelanden
 //   searchResultDiv.innerHTML = '';
 //   errorH3.textContent = '';
 
+//   // Visa stadens namn
 //   cityNameElement.textContent = weather.city.name;
 
 //   // Skapa element för nuvarande väder
 //   const currentWeatherContainer = document.createElement('div');
 //   currentWeatherContainer.classList.add('forecast-item');
 
+//   // Skapa element för nuvarande väderbeskrivning
 //   const currentWeatherTitle = document.createElement('h3');
 //   currentWeatherTitle.textContent = 'Current Weather';
 //   currentWeatherContainer.append(currentWeatherTitle);
 
+//   // Skapa element för temperatur, luftfuktighet och vindhastighet
 //   const currentWeatherTemperature = document.createElement('p');
 //   currentWeatherTemperature.textContent = `Temp: ${weather.list[0].main.temp} °C`;
 //   currentWeatherContainer.append(currentWeatherTemperature);
@@ -238,6 +269,7 @@ function fetchWeatherByCity(city) {
 //   currentWeatherWindSpeed.textContent = `Wind Speed: ${weather.list[0].wind.speed} m/s`;
 //   currentWeatherContainer.append(currentWeatherWindSpeed);
 
+//   // Visa ikon och beskrivning för nuvarande väder
 //   const currentWeatherInfo = weather.list[0].weather[0];
 //   if (currentWeatherInfo && currentWeatherInfo.icon) {
 //     const iconUrl = `https://openweathermap.org/img/wn/${currentWeatherInfo.icon}.png`;
@@ -249,9 +281,10 @@ function fetchWeatherByCity(city) {
 //     currentWeatherContainer.append(iconInfo);
 //   }
 
+//   // Lägg till nuvarande väder i resultatet
 //   searchResultDiv.append(currentWeatherContainer);
 
-  
+//   // Skapa väderprognos för varje intervall
 //   for (let i = 3; i <= forecastInterval; i += 3) {
 //     const forecastItem = weather.list[(i / 3) - 1];
 //     const forecastItemTime = new Date(forecastItem.dt * 1000);
@@ -290,18 +323,23 @@ function fetchWeatherByCity(city) {
 //       forecastItemContainer.append(iconInfo);
 //     }
 
+//     // Lägg till väderprognos i resultatet
 //     searchResultDiv.append(forecastItemContainer);
 //   }
 
+//   // Visa resultatet
 //   searchResultDiv.style.display = 'flex';
 // }
 
+// // Funktion för att visa felmeddelande
 // function showErrorMessage(message) {
+//   // Rensa tidigare resultat och felmeddelanden
 //   searchResultDiv.innerHTML = '';
 //   errorH3.textContent = message;
 //   searchResultDiv.style.display = 'none';
 // }
 
+// // Funktion för att hämta väderdata
 // function fetchWeather(event) {
 //   event.preventDefault();
 //   searchResultDiv.innerHTML = '';
@@ -323,17 +361,19 @@ function fetchWeatherByCity(city) {
 //     .catch((error) => {
 //       console.log(error);
 //       showErrorMessage('City not found');
-//       searchResultDiv.innerHTML = ''; 
-//       cityNameElement.textContent = ''; 
+//       searchResultDiv.innerHTML = '';
+//       cityNameElement.textContent = '';
 //     });
 // }
 
+// // Funktion för att öka väderintervallet
 // function increaseInterval() {
 //   forecastInterval += 3;
 //   updateIntervalInput();
 //   fetchWeatherByCity(city);
 // }
 
+// // Funktion för att minska väderintervallet
 // function decreaseInterval() {
 //   if (forecastInterval - 3 >= minForecastInterval) {
 //     forecastInterval -= 3;
@@ -342,6 +382,7 @@ function fetchWeatherByCity(city) {
 //   }
 // }
 
+// // Funktion för att uppdatera väderintervallet
 // function updateInterval() {
 //   const newForecastInterval = parseInt(intervalInput.value);
 //   if (newForecastInterval >= minForecastInterval) {
@@ -350,10 +391,12 @@ function fetchWeatherByCity(city) {
 //   }
 // }
 
+// // Funktion för att uppdatera väderintervallets inputfält
 // function updateIntervalInput() {
 //   intervalInput.value = forecastInterval.toString();
 // }
 
+// // Funktion för att hämta väderdata med en specifik stad
 // function fetchWeatherByCity(city) {
 //   const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=e26f81e1cb4b35fdc36b4b6cd3ea2ca5`;
 
@@ -371,19 +414,7 @@ function fetchWeatherByCity(city) {
 //     .catch((error) => {
 //       console.log(error);
 //       showErrorMessage('City not found');
-//       searchResultDiv.innerHTML = ''; 
-//       cityNameElement.textContent = ''; 
+//       searchResultDiv.innerHTML = '';
+//       cityNameElement.textContent = '';
 //     });
 // }
-
-
-
-
-
-
-
-
-
-
-
-
